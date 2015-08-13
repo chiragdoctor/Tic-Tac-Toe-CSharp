5f7ff58fc59af20c71fbb49c5edd48da61849d6a
chiragdoctor <chirag.doctor@gmail.com>
2015-08-13 19:32:11 +0100

    determine while player has won

---
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Match.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tic_Tac_Toe_CSharp
+{
+    public interface IMatch
+    {
+        State DetermineWinState(List<string> cells, IGamePlay gamePlay);
+        void Result(State matchState, int player);
+    }
+
+    public class Match : IMatch
+    {
+        public State DetermineWinState(List<string> cells, IGamePlay gamePlay)
+        {
+
+            return gamePlay.GetState(cells);
+
+        }
+
+        public void Result(State matchState, int player)
+        {
+            if (matchState == State.Win)
+            {
+                Console.WriteLine("Computer {0} has won", (player % 2) + 1);
+            }
+
+            else
+            {
+                Console.WriteLine("Draw");
+            }
+        }
+    }
+}
