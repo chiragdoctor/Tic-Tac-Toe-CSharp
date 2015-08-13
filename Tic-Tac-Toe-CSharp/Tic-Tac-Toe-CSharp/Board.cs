18f46bea5c4c7e6038c564e9a7a31939a21367ee
chiragdoctor <chirag.doctor@gmail.com>
2015-08-13 16:56:09 +0100

    refactor game board from program

---
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Board.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tic_Tac_Toe_CSharp
+{
+    public static class Board
+    {
+        public static void Create(List<string> cell)
+        {
+            Console.WriteLine("Press any Key to start game: ");
+            Console.ReadKey();
+
+            Console.WriteLine("Computer1 : X & Computer2 : O");
+            Console.WriteLine("\n");
+
+            Console.WriteLine(" __________________ ");
+            Console.WriteLine("|     |     |      |");
+            Console.WriteLine("|  {0}  |  {1}  |  {2}", cell[1], cell[2], cell[3]);
+            Console.WriteLine("|_____|_____|_____ |");
+            Console.WriteLine("|     |     |      |");
+            Console.WriteLine("|  {0}  |  {1}  |  {2}", cell[4], cell[5], cell[6]);
+            Console.WriteLine("|_____|_____|_____ |");
+            Console.WriteLine("|     |     |      |");
+            Console.WriteLine("|  {0}  |  {1}  |  {2}", cell[7], cell[8], cell[9]);
+            Console.WriteLine("|     |     |      |");
+            Console.WriteLine(" __________________ ");
+
+            Console.ReadKey();
+        }
+    }
+}
