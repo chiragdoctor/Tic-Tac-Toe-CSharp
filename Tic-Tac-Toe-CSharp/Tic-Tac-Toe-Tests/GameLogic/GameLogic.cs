25341770cf32b6090b2f14aadaa4401dbaf982ba
chiragdoctor <chirag.doctor@gmail.com>
2015-08-13 19:04:34 +0100

    adding draw state to game logic

--- Tic-Tac-Toe-CSharp/Tic-Tac-Toe-Tests/GameLogic/GameLogic.cs
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-Tests/GameLogic/GameLogic.cs
@@ -37,4 +37,15 @@ namespace Tic_Tac_Toe_Tests.GameLogic
             return State.NoState;
         }
     }
+
+    public class DrawState : IGameLogic
+    {
+        public State GetState(List<string> cells)
+        {
+            if (cells[1] != "1" && cells[2] != "2" && cells[3] != "3" && cells[4] != "4" && cells[5] != "5" && cells[6] != "6" &&
+                     cells[7] != "7" && cells[8] != "8" && cells[9] != "9")
+                return State.Draw;
+            return State.NoState;
+        }
+    }
 }
