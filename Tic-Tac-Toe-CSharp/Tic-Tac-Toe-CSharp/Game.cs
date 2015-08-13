f4fbfe4615569f145271e968f93093c6ba0d1ca3
chiragdoctor <chirag.doctor@gmail.com>
2015-08-13 17:00:49 +0100

    creating game class and creating board in it

---
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe_CSharp
+{
+    public class Game
+    {
+        private static readonly List<string> cells = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        public void Execute()
+        {
+            Board.Create(cells);
+        }
+    }
+}
--- Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Program.cs
+++ Tic-Tac-Toe-CSharp/Tic-Tac-Toe-CSharp/Program.cs
@@ -8,10 +8,10 @@ namespace Tic_Tac_Toe_CSharp
 {
     class Program
     {
-        private static readonly List<string> cells = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+       
         static void Main(string[] args)
         {
-            Board.Create(cells);
+            new Game().Execute();
         }
     }
 }
